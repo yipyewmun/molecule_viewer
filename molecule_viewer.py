316571ad9c4770a707b60e125e4b35b71684670d
Yew Mun <yipyewmun@outlook.com>
2021-12-16 20:04:49 +0800

    Added 'Previous' and 'Next' buttons

--- molecule_viewer.py
+++ molecule_viewer.py
@@ -17,6 +17,10 @@ from rdkit import DataStructs
 def get_data():
     return []
 
+@st.cache(allow_output_mutation=True)
+def get_idx():
+    return [0]
+
 sideb=st.sidebar
 
 # Set header title
@@ -34,7 +38,7 @@ if compare_molecules:
             smiles_2 = df['SMILES_2'].tolist()
             
             # selected_smiles_1 = sideb.selectbox('Select SMILES to visualize', smiles_1)
-            selected_smiles_1 = sideb.selectbox('Select SMILES to visualize', range(len(smiles_1)), format_func=lambda x: smiles_1[x])
+            selected_smiles_1 = sideb.selectbox('Select Molecule Pair to visualize (' + str(len(smiles_1)) + ' pairs in total)' , range(len(smiles_1)), format_func=lambda x: x+1)
             selected_smiles_2 = smiles_2[selected_smiles_1]
 
             mol_1 = Chem.MolFromSmiles(smiles_1[selected_smiles_1])
@@ -54,9 +58,13 @@ if compare_molecules:
                 png_file.write(img_2)
 
             tanimoto_similarity = DataStructs.FingerprintSimilarity(Chem.RDKFingerprint(mol_1), Chem.RDKFingerprint(mol_2))
-            st.write('Tanimoto Similarity: ' + str(tanimoto_similarity))
-            st.image('tmp/mol_image_1.png')
-            st.image('tmp/mol_image_2.png')
+            st.write(str(selected_smiles_1+1) + '. Tanimoto Similarity: ' + str(tanimoto_similarity))
+
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.image('tmp/mol_image_1.png')
+            with col2:
+                st.image('tmp/mol_image_2.png')
 
             add_to_list = sideb.button('Add to list?')
             if add_to_list:
@@ -106,18 +114,67 @@ else:
         df = pd.read_csv(uploaded_file)
         smiles_lst = df['SMILES'].tolist()
 
-        selected_SMILES = sideb.selectbox('Select SMILES to visualize', smiles_lst)
-        mol = Chem.MolFromSmiles(selected_SMILES)
-        d2d = rdMolDraw2D.MolDraw2DCairo(350,300)
-        d2d.DrawMolecule(mol)
-        d2d.FinishDrawing()
-        img = d2d.GetDrawingText()
+        # selected_SMILES = sideb.selectbox('Select SMILES to visualize', smiles_lst)
+        # mol = Chem.MolFromSmiles('')
+        # d2d = rdMolDraw2D.MolDraw2DCairo(350,300)
+        # d2d.DrawMolecule(mol)
+        # d2d.FinishDrawing()
+        # img = d2d.GetDrawingText()
 
+        col1, col2 = st.columns([1,1])
+        with col1:
+            previous_structure = st.button('Previous')
+        with col2:
+            next_structure = st.button('Next')
+        
+        if previous_structure:
+            # Get previous index
+            prev_idx = get_idx()[-1] - 1
+            if prev_idx < 0:
+                pass
+            elif prev_idx == 0:
+                mol = Chem.MolFromSmiles(smiles_lst[prev_idx])
+                d2d = rdMolDraw2D.MolDraw2DCairo(350,300)
+                d2d.DrawMolecule(mol)
+                d2d.FinishDrawing()
+                img = d2d.GetDrawingText()
+
+                print(prev_idx)
+                get_idx().append(prev_idx)
+                print(get_idx())
+            else:
+                mol = Chem.MolFromSmiles(smiles_lst[prev_idx])
+                d2d = rdMolDraw2D.MolDraw2DCairo(350,300)
+                d2d.DrawMolecule(mol)
+                d2d.FinishDrawing()
+                img = d2d.GetDrawingText()
+
+                # print(prev_idx)
+                # get_idx().append(prev_idx)
+                # print(get_idx())
+
+        if next_structure:
+            # Get next index
+            next_idx = get_idx()[-1] + 1
+
+            mol = Chem.MolFromSmiles(smiles_lst[next_idx])
+            d2d = rdMolDraw2D.MolDraw2DCairo(350,300)
+            d2d.DrawMolecule(mol)
+            d2d.FinishDrawing()
+            img = d2d.GetDrawingText()
+
+            # print(next_idx)
+            # get_idx().append(next_idx)
+            # print(get_idx())
+        
         with open('tmp/mol_image.png', 'wb') as png_file:
             png_file.write(img)
 
         st.image('tmp/mol_image.png')
     else:
+        get_idx().clear()
+        get_idx().append(0)
+
         query = sideb.text_input("Please enter your SMILES here:")
         mol = Chem.MolFromSmiles(query)
         d2d = rdMolDraw2D.MolDraw2DCairo(350,300)
